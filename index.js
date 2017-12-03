25c9a7e1681c88b7be11cfe63eed2adecbcf7b00
George Kushnir <admin@n4ru.it>
2017-12-03 17:57:30 -0500

    Basically rewrote the whole thing.

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-/*
-
-jData by n4ru
-
-*/
-
 const fs = require('fs');
-const databaseObj = {};
 
-databaseObj._name = null;
-
-databaseObj._use = (database) => {
-    if (!database)
-        database = 'db';
-    databaseObj._name = database;
-    if (fs.existsSync(database + '.json')) {
-        fs.readFile(database + '.json', 'utf-8', (err, data) => {
-            Object.assign(databaseObj, JSON.parse(data))
-            if (err) throw err;
-        });
-    } else {
-        fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
-            if (err) throw err;
-        });
+class database {
+    constructor(database, object) {
+        this.name = database;
+        if (object) {
+            this.db = object;
+            fs.writeFileSync(this.name, JSON.stringify(object));
+        } else {
+            if (fs.existsSync(database)) {
+                this.db = JSON.parse(fs.readFileSync(this.name, 'utf-8'));
+            } else {
+                this.db = {};
+            }
+        }
+        return new Proxy(this.db, this);
     }
+
+    set(target, key, value) {
+        this.db[key] = value;
+        fs.writeFileSync(this.name, JSON.stringify(this.db));
+    }
+
 }
 
-const handler = {
-    set(target, key, value) {
-        target[key] = value;
-        if (!databaseObj._name && !database)
-            if (databaseObj._name && !database)
-                database = 'db';
-        fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
-            if (err) throw err;
-        });
-    },
-};
-
-var db = new Proxy(databaseObj, handler);
-
-module.exports = db;
+module.exports = database;
