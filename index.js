37164ba8f1dd4ec3ac281408c4ed39ac73be5faf
George Kushnir <admin@n4ru.it>
2017-12-03 12:51:48 -0500

    Remove save & promises since it's all synchronous.

--- index.js
+++ index.js
@@ -4,53 +4,36 @@ jData by n4ru
 
 */
 
-var fs = require('fs');
-var databaseObj = {};
-var _name = null;
+const fs = require('fs');
+const databaseObj = {};
 
-
-databaseObj._save = (database) => {
-    return new Promise((res, rej) => {
-        if (!_name && !database)
-            rej("No database specified.");
-        if (_name && !database)
-            database = _name;
-        fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
-            if (err)
-                rej(err);
-            else
-                res();
-        });
-    });
-};
+databaseObj._name = null;
 
 databaseObj._use = (database) => {
-    return new Promise((res, rej) => {
-        if (!database)
-            rej("No database specified.")
-        _name = database;
-        if (fs.existsSync(database + '.json')) {
-            fs.readFile(database + '.json', 'utf-8', (err, data) => {
-                console.log('reading file')
-                Object.assign(databaseObj, JSON.parse(data))
-                if (err) throw err;
-                res();
-            });
-        } else {
-            fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
-                if (err)
-                    rej(err);
-                else
-                    res();
-            });
-        }
-    });
+    if (!database)
+        database = 'db';
+    databaseObj._name = database;
+    if (fs.existsSync(database + '.json')) {
+        fs.readFile(database + '.json', 'utf-8', (err, data) => {
+            Object.assign(databaseObj, JSON.parse(data))
+            if (err) throw err;
+        });
+    } else {
+        fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
+            if (err) throw err;
+        });
+    }
 }
 
 const handler = {
     set(target, key, value) {
         target[key] = value;
-        databaseObj._save();
+        if (!databaseObj._name && !database)
+            if (databaseObj._name && !database)
+                database = 'db';
+        fs.writeFile(database + '.json', JSON.stringify(databaseObj), (err) => {
+            if (err) throw err;
+        });
     },
 };
 
